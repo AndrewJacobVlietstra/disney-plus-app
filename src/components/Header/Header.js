52afcb3b43c81e44a047dad483bfec18af0a699a
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-03-16 16:01:35 -0400

    Setup base project with redux-template, removed base styling and some files and started working on the header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.styles.scss';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      Hi, I'm the Header Component
+    </div>
+  )
+}
+
+export default Header;
