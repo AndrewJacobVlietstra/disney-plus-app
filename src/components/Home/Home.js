e4b7eb82973a49e6cf2859bd9b666d1713ff1492
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-03-17 13:16:08 -0400

    Tweaked styling for Home and Header, started working on Viewers component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Home.styles.scss';
 import ImageSlider from '../ImageSlider/ImageSlider';
+import Viewers from '../Viewers/Viewers';
 
 const Home = () => {
   return (
     <main className='home-container'>
       <ImageSlider />
+      <Viewers />
     </main>
   )
 }
---
+++ src/components/Viewers/Viewers.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Viewers.styles.scss';
+
+const Viewers = () => {
+  return (
+    <div className='viewers-container'>
+      Viewers Component
+    </div>
+  )
+}
+
+export default Viewers;
