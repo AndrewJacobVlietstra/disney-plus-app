e91262851b81b0d1fdabfef147660d80c252ea62
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-03-17 16:57:12 -0400

    Finished styling the base layout of viewers component, still need to add the movie hover effects though

--- src/components/Viewers/Viewers.js
+++ src/components/Viewers/Viewers.js
@@ -4,7 +4,27 @@ import './Viewers.styles.scss';
 const Viewers = () => {
   return (
     <div className='viewers-container'>
-      Viewers Component
+
+      <div className='viewer-wrapper'>
+        <img className='viewer-image' src='/images/viewers-disney.png' />
+      </div>
+
+      <div className='viewer-wrapper'>
+        <img className='viewer-image' src='/images/viewers-pixar.png' />
+      </div>
+
+      <div className='viewer-wrapper'>
+        <img className='viewer-image' src='/images/viewers-marvel.png' />
+      </div>
+
+      <div className='viewer-wrapper'>
+        <img className='viewer-image' src='/images/viewers-starwars.png' />
+      </div>
+
+      <div className='viewer-wrapper'>
+        <img className='viewer-image' src='/images/viewers-national.png' />
+      </div>
+
     </div>
   )
 }
