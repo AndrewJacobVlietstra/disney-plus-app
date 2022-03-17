75599b32f62f2d48178eabde5e1d1f8ffacdcc15
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-03-17 12:03:33 -0400

    Installed and setup a simple react-slick slider carousel in my ImageSlider component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Home.styles.scss';
+import ImageSlider from '../ImageSlider/ImageSlider';
 
 const Home = () => {
   return (
     <main className='home-container'>
       Home Component
+      <ImageSlider />
     </main>
   )
 }
--- src/components/ImageSlider/ImageSlider.js
+++ src/components/ImageSlider/ImageSlider.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './ImageSlider.styles.scss';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
 
 const ImageSlider = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    arrows: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4500,
+  };
+
   return (
-    <div className='image-slider-container'>
-      ImageSlider Component
-    </div>
+    <Slider {...settings} className='image-slider-container'>
+      <h1>Hi</h1>
+      <h1>Yo</h1>
+      <h1>Bye</h1>
+    </Slider>
   )
 }
 
