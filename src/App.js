02187968b91a8eaa0ccc64edb59f279fcf71c730
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-03-18 10:36:24 -0400

    Finished styling background for login page, now need to add in logos and subscription button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Detail from './components/Detail/Detail';
+import Login from './components/Login/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Router>
         <Header />
         <Routes>
+          <Route path='/login' element={<Login />} />
           <Route path='/' element={<Home />} />
           <Route path='/detail' element={<Detail />} />
         </Routes>
---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.styles.scss';
+
+const Login = () => {
+  return (
+    <div className='login-page-container'>
+      Login Page
+    </div>
+  )
+}
+
+export default Login;
