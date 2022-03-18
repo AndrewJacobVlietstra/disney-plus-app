95b1e1fbb7eb0341a1468987cd4c9d74a35198da
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-03-18 11:03:35 -0400

    Finished styling login page, logos, button and sign-up description

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,7 +4,17 @@ import './Login.styles.scss';
 const Login = () => {
   return (
     <div className='login-page-container'>
-      Login Page
+      <div className='login-content-cta'>
+        <img src='/images/cta-logo-one.svg' className='login-logo-1 login-logo' />
+        <div className='sign-up'>
+          <button>get all there</button>
+          <p className='sign-up-description'>
+            Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+ subscription. 
+            As of 03/26/21, the price of Disney+ and The Disney Bundle will increase by $1.
+          </p>
+        </div>
+        <img src='/images/cta-logo-two.png' className='login-logo-2 login-logo' />
+      </div>
     </div>
   )
 }
