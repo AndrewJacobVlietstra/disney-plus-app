66d5871585ebb34dff5b6b231687cc5bd742a3fe
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-03-21 00:38:54 -0400

    modified starwars query to pull in more star wars things and less indiana jones things

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -11,15 +11,16 @@ const Showcase = () => {
   let companyID;
   let imageSrc;
   let videoSrc;
+  let genres = '';
   if (currentShowcase === 'disney') {companyID = '2'; imageSrc = 'images/viewers-disney.png'; videoSrc = 'videos/disney-intro.mp4';}
   if (currentShowcase === 'pixar') {companyID = '3'; imageSrc = 'images/viewers-pixar.png'; videoSrc = 'videos/pixar-intro.mp4';}
   if (currentShowcase === 'marvel') {companyID = '420'; imageSrc = 'images/viewers-marvel.png'; videoSrc = 'videos/marvel-intro.mp4';}
-  if (currentShowcase === 'star-wars') {companyID = '1'; imageSrc = 'images/viewers-starwars.png'; videoSrc = 'videos/star-wars-intro.mp4';}
+  if (currentShowcase === 'star-wars') {companyID = '1'; imageSrc = 'images/viewers-starwars.png'; videoSrc = 'videos/star-wars-intro.mp4'; genres = '&with_genres=878'}
   if (currentShowcase === 'national-geographic') {companyID = '7521'; imageSrc = 'images/viewers-national.png'; videoSrc = 'videos/national-intro.mp4';}
   if (currentShowcase === 'star') {companyID = '1556'; imageSrc = 'images/viewers-star.png'; videoSrc = 'videos/fox-intro.mp4';}
   
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/discover/movie?${API_KEY}language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_companies=${companyID}`)
+    fetch(`https://api.themoviedb.org/3/discover/movie?${API_KEY}language=en-US&sort_by=popularity.desc&include_adult=false&page=1&with_companies=${companyID}${genres}`)
       .then(res => res.json())
       .then(data => setShowCaseMovies(data?.results))
       .catch(err => console.error(err));
