05409b0db40ec6147b0973ae8029b7b651d17120
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-03-24 18:41:53 -0400

    Added classes to showcase to control positioning of individual BG videos more effectively

--- src/components/Showcase/Showcase.js
+++ src/components/Showcase/Showcase.js
@@ -11,13 +11,14 @@ const Showcase = () => {
   let companyID;
   let imageSrc;
   let videoSrc;
+  let videoClass = '';
   let genres = '';
-  if (currentShowcase === 'disney') {companyID = '2'; imageSrc = 'images/viewers-disney.png'; videoSrc = 'videos/disney-intro.mp4';}
-  if (currentShowcase === 'pixar') {companyID = '3'; imageSrc = 'images/viewers-pixar.png'; videoSrc = 'videos/pixar-intro.mp4';}
-  if (currentShowcase === 'marvel') {companyID = '420'; imageSrc = 'images/viewers-marvel.png'; videoSrc = 'videos/marvel-intro.mp4';}
-  if (currentShowcase === 'star-wars') {companyID = '1'; imageSrc = 'images/viewers-starwars.png'; videoSrc = 'videos/star-wars-intro.mp4'; genres = '&with_genres=878'}
-  if (currentShowcase === 'national-geographic') {companyID = '7521'; imageSrc = 'images/viewers-national.png'; videoSrc = 'videos/national-intro.mp4';}
-  if (currentShowcase === 'star') {companyID = '25'; imageSrc = 'images/viewers-star.png'; videoSrc = 'videos/fox-intro.mp4';}
+  if (currentShowcase === 'disney') {companyID = '2'; imageSrc = 'images/viewers-disney.png'; videoSrc = 'videos/disney-intro.mp4'; videoClass = 'disneyBG';}
+  if (currentShowcase === 'pixar') {companyID = '3'; imageSrc = 'images/viewers-pixar.png'; videoSrc = 'videos/pixar-intro.mp4'; videoClass = 'pixarBG';}
+  if (currentShowcase === 'marvel') {companyID = '420'; imageSrc = 'images/viewers-marvel.png'; videoSrc = 'videos/marvel-intro.mp4'; videoClass = 'marvelBG';}
+  if (currentShowcase === 'star-wars') {companyID = '1'; imageSrc = 'images/viewers-starwars.png'; videoSrc = 'videos/star-wars-intro.mp4'; videoClass = 'starwarsBG'; genres = '&with_genres=878'}
+  if (currentShowcase === 'national-geographic') {companyID = '7521'; imageSrc = 'images/viewers-national.png'; videoSrc = 'videos/national-intro.mp4'; videoClass = 'nationalBG';}
+  if (currentShowcase === 'star') {companyID = '25'; imageSrc = 'images/viewers-star.png'; videoSrc = 'videos/fox-intro.mp4'; videoClass = 'foxBG';}
   
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/discover/movie?${API_KEY}language=en-US&sort_by=popularity.desc&include_adult=false&vote_average.gte=6&page=1&with_companies=${companyID}${genres}`)
@@ -28,7 +29,7 @@ const Showcase = () => {
   
   return (
     <div className='showcase-page-container'>
-      <div className='showcase-bg'>
+      <div className={`showcase-bg ${videoClass}`}>
         <video className='bg-video' autoPlay muted loop>
           <source src={videoSrc} />
         </video>
