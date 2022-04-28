c4c2105678072c6e037940a79914d31d27e4da17
Andrew Vlietstra <vlietstra123@hotmail.com>
2022-04-28 16:57:10 -0400

    moved 404.html into the public folder and out of root directory

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Header_Mobile from './components/Header_Mobile/Header_Mobile';
 import Login from './components/Login/Login';
